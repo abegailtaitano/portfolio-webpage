f9c15b98732e49503d31c7dbecf0c6a4ee700518
Abegail Taitano <79725435+abegailtaitano@users.noreply.github.com>
2022-01-22 16:55:50 -0800

    link-component

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ const About = () => {
       <>
       <h1>Hello Abby!</h1>
       <p>this is your about me page?</p>
+      <Link to="/">Back to Home</Link>
       </>
     );
   };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
-import * as React from "react"
+import * as React from 'react'
+import { Link } from 'gatsby'
+
 
 const Homepage = () => {
   return (
     <>
     <h1>Hello Abby!</h1>
     <p>How are you?</p>
+    <Link to="/about">About page</Link>
     </>
   );
 };
