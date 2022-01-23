79402ede4313f632012fe7aafae080babca5aa29
Abegail Taitano <79725435+abegailtaitano@users.noreply.github.com>
2022-01-22 16:13:06 -0800

    index-and-about-updated

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const About = () => {
+    return (
+      <>
+      <h1>Hello Abby!</h1>
+      <p>this is your about me page?</p>
+      </>
+    );
+  };
+
+  
+  export default About;
