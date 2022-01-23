61840a4fdc343ed66806f7963ddba5fcd6a8ae68
Abegail Taitano <79725435+abegailtaitano@users.noreply.github.com>
2022-01-22 17:32:00 -0800

    contact-page

---
+++ components/layout.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const Layout = () => {
+    return (
+        <main>
+        <title>...</title>
+        <h1>Welcome</h1>
+        </main>
+    )
+}
+
+
+export default Layout;
---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from 'components/Layout'
+
+const Contactpage = () => {
+  return (
+    <main>
+    <title>...</title>
+    <h1>Welcome</h1>
+    </main>
+
+    
+  );
+};
+
+
+export default Contactpage;
