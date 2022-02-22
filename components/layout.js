f965480252647e32b8fbff092717479dced03ae5
Abegail Taitano <79725435+abegailtaitano@users.noreply.github.com>
2022-02-22 17:56:34 -0600

    images-config-attempt

--- components/layout.js
+++ components/layout.js
@@ -4,6 +4,7 @@ import { container, heading, navLinks, navLinkItem, navLinkText } from './layout
 
 console.log('container: ', container)
 
+
 const Layout = ({ pageTitle, pageHeading, children }) => {
   return (
     <main className={container}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../../components/layout'
 
 const Homepage = () => {
   return (
     <Layout pageHeading = 'Home' pageTitle='Site Home'>
-     Coding is fun and you will learn to love it.
+     <p>Coding is fun and you will learn to love it.</p>
    </Layout>
   )
 }
